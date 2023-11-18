34baf4e7e4a7ae8c48e9374323ac7576d15593e7
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-18 16:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+// script.js
+
+document.getElementById("fname").addEventListener("blur", function() {
+    var inputField = document.getElementById("fname");
+    inputField.value = inputField.value.toUpperCase();
+});
